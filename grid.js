d718d2f5ee0ead01ac63577de2abad823d422ef8
Phillip Jackson <unisys12@gmail.com>
2013-02-25 18:48:07 -0600

    Update grid.js
    Removed window.onload from the file. This allows for easier use with
    other canvas files. 

--- grid.js
+++ grid.js
@@ -1,50 +1,38 @@
-window.onload = function(){
+function init(){
 
-  function init(){
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
+height = canvas.clientHeight;
+width = canvas.clientWidth;
+inc = 10; // change this to adjust your grid spacing!!
+}
 
-	canvas = document.getElementById('canvas');
-	ctx = canvas.getContext('2d');
-	height = canvas.clientHeight;
-	width = canvas.clientWidth;
-	inc = 10; // change this to adjust your grid spacing!!
+init();
+
+function drawGrid(){
+
+	for (var h = 0; h < width; h++) {
+		if(h % inc === 0){
+			ctx.beginPath();
+			ctx.moveTo(h, 0);
+			ctx.lineTo(h, height);
+			ctx.closePath();
+			ctx.strokeStyle = "rgba(0,0,0,.2)";
+			ctx.stroke();
+		}
 	}
 
-	init();
-
-	function drawGrid(){
-
-		for (var h = 0; h < width; h++) {
-			if(h % inc === 0){
-				ctx.beginPath();
-				ctx.moveTo(h, 0);
-				ctx.lineTo(h, height);
-				ctx.closePath();
-				ctx.strokeStyle = "rgba(0,0,0,.2)";
-				ctx.stroke();
-			}
+	for (var l = inc; l < width; l++) {
+		if(l % inc === 0){
+			ctx.beginPath();
+			ctx.moveTo(0, l);
+			ctx.lineTo(width, l);
+			ctx.closePath();
+			ctx.stroke();
 		}
-
-		for (var l = inc; l < width; l++) {
-			if(l % inc === 0){
-				ctx.beginPath();
-				ctx.moveTo(0, l);
-				ctx.lineTo(width, l);
-				ctx.closePath();
-				ctx.stroke();
-			}
-		}
-
-
 	}
 
-	drawGrid();
-	
-	/** Your code goes below here!! Then, when you 
-	 *  are through using the grid, just create a new
-	 *  file for your code, with your canvas var and 
-	 *  paste your code there. This is rough right
-	 *  now, so I am going to work on a better 
-	 *  solution.
-	 **/
 
-};
+}
+
+drawGrid();
